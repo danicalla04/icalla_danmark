56599c9909ebc526fa32826a7109b678c2f9c74b
icalla_danmark <icalladan@gmail.com>
2025-09-12 22:25:20 +0800

    baseurl

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'production';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'http://localhost:3000/';
+$config['base_url'] 				= 'https://icalla-danmark-e3ri.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
