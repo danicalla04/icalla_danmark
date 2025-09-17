0ddecf0f41aae47611647939d9d614bcab5abc14
icalla_danmark <icalladan@gmail.com>
2025-09-17 19:07:05 +0800

    error on get env

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 define('PREVENT_DIRECT_ACCESS', TRUE);
 /**
  * ------------------------------------------------------------------
