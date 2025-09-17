68356f23d55de60b1a2bb37fb9d4fc72f3b22324
icalla_danmark <icalladan@gmail.com>
2025-09-17 19:00:59 +0800

    getenv

--- app/config/database.php
+++ app/config/database.php
@@ -58,15 +58,13 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'	=> 'mysql',
-    'hostname'	=> 'sql12.freesqldatabase.com',
-    'port'		=> '3306',
-    'username'	=> 'sql12798368',
-    'password'	=> 'pzwrzyiz1p',
-    'database'	=> 'sql12798368',
-    'charset'	=> 'utf8mb4',
-    'dbprefix'	=> '',
-    'path'      => ''
+    'driver'   => 'mysql',
+    'hostname' => getenv('DB_HOST'),
+    'port'     => getenv('DB_PORT'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'database' => getenv('DB_NAME'),
+    'charset'  => 'utf8mb4',
+    'dbprefix' => '',
+    'path'     => ''
 );
-
-?>
