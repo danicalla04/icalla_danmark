f23ebda3e234cf4268ee8361639b2d961bcf2cc9
icalla_danmark <icalladan@gmail.com>
2025-09-19 15:16:30 +0800

    pagination 1

--- app/views/View.php
+++ app/views/View.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View</title>
-    <link rel="stylesheet" href="<?= base_url(); ?>public/style.css">
+
 </head>
 <body>
     <div>
