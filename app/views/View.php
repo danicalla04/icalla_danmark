4f7e15d89ed32426033e81446483c38401bbd6ad
icalla_danmark <icalladan@gmail.com>
2025-09-20 12:06:24 +0800

    adding design

--- app/views/View.php
+++ app/views/View.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url(); ?>public/style.css">
     <title>View</title>
 
 </head>
-<body>
-  
+<body>  
     <table border=1>
         <tr>
             <th>ID</th>
